72c27788471fc8fe1bebba40786934d60077233e
Steven Knight <steven@knight.cx>
2014-07-09 11:46:05 -0400

    Added initial version of vm and bytecodes.

---
+++ lib/bytecodes.rb
@@ -0,0 +1,26 @@
+require 'ruby_enum'
+
+class Bytecodes
+
+  include Ruby::Enum
+
+  define :IADD, 1
+  define :ISUB, 2
+  define :IMUL, 3
+  define :IIL, 4
+  define :IEQ, 5
+  define :BR, 6
+  define :BRT, 7
+  define :BRF, 8
+  define :ICONST, 9
+  define :LOAD, 10
+  define :GLOAD, 11
+  define :STORE, 12
+  define :GSTORE, 13
+  define :PRINT, 14
+  define :POP, 15
+  define :CALL, 16
+  define :RET, 17
+  define :HALT, 18
+
+end

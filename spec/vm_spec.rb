2cde9703cbb2fa8bf0db48511aa372d79f699522
Steven Knight <steven@knight.cx>
2014-07-09 18:32:27 -0400

    Added another test.  Renamed VM#cpu to VM#exec.

--- spec/vm_spec.rb
+++ spec/vm_spec.rb
@@ -3,16 +3,19 @@ require 'vm'
 require 'bytecodes'
 require 'pp'
 
+
+VM.disable_tracking_globally()
+
 describe "VM tests:" do
   it "should halt" do
     vm = VM.new([Bytecodes::HALT])
-    rv = vm.cpu
+    rv = vm.exec
     expect(rv.opcode).to eq(Bytecodes::HALT.opcode)
   end
 
   context "it should load and halt" do
     vm = VM.new([Bytecodes::ICONST, 100, Bytecodes::HALT])
-    rv = vm.cpu
+    rv = vm.exec
 
     it "should load 100" do
       expect(vm.stack[vm.sp]).to eq(100)
@@ -25,7 +28,7 @@ describe "VM tests:" do
 
   context "it should load 2 constants and halt" do
     vm = VM.new([Bytecodes::ICONST, 1, Bytecodes::ICONST, 2, Bytecodes::HALT])
-    rv = vm.cpu
+    rv = vm.exec
     #puts "VM #{vm}"
 
     it "should load 1 and 2" do
@@ -37,5 +40,26 @@ describe "VM tests:" do
       expect(rv).to eq(Bytecodes::HALT)
     end
   end
+
+  context "it should load 2 constants, print and halt" do
+    vm = VM.new([Bytecodes::ICONST, 1, Bytecodes::ICONST, 2,
+      Bytecodes::PRINT,
+      Bytecodes::HALT])
+    rv = vm.exec
+
+    it "should load 1 and 2, " do
+      expect(vm.stack[vm.sp-2]).to eq(1)
+      expect(vm.stack[vm.sp-1]).to eq(2)
+    end
+
+    it "print " do
+     expect(vm.code).to include(Bytecodes::PRINT)
+    end
+
+    it "and halt" do
+      expect(rv).to eq(Bytecodes::HALT)
+    end
+  end
+
 
 end

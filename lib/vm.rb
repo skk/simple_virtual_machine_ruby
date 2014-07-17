a0f21d31d7794a3c772f60556008426f5e1c7551
Steven Knight <steven@knight.cx>
2014-07-16 23:49:13 -0400

    Corrected (some) issues reported by rubocop. Removed global variable Tracing; moved to class variable.

--- lib/vm.rb
+++ lib/vm.rb
@@ -1,6 +1,4 @@
-
 class VM
-
   # instruction pointer
   attr_accessor :ip
   # frame pointer
@@ -15,60 +13,43 @@ class VM
   # stack storage
   attr_accessor :stack    # aka stack
 
-  attr_accessor :tracing
-
-  at_exit { puts TRACE_OUTPUT.join("") }
+  at_exit { puts TRACE_OUTPUT.join('') }
   TRACE_OUTPUT = []
 
-  $TracingGlobally = false
-
-  def initialize(code, ip = 0, datasize = 0)
+  def initialize(code, ip = 0, _datasize = 0)
     @code = code
     @ip = ip
     @fp = 0
     @sp = -1
-    @data = Array.new()
-    @stack = Array.new()
+    @data = []
+    @stack = []
     @tracing = false
-    trace { ("-" * 60) + "\n" }
-    #trace { self }
+    trace { ('-' * 60) + "\n" }
+    # trace { self }
   end
-
-
-  def self.disable_tracking_globally
-    $TracingGlobally = false
-  end
-
-  def self.enable_tracking_globally
-    $TracingGlobally = true
-  end
-
 
   def trace
-    if @tracing || $TracingGlobally
+    if VM.tracing
       TRACE_OUTPUT << yield
     end
   end
 
-
   def format_instr_or_object(o)
-    if o.kind_of?(Instruction)
+    if o.is_a?(Instruction)
       Bytecodes.to_instruction_from_opcode(o.opcode)
     else
       "#{o}"
     end
   end
 
-
   def to_s
     buf = "FP #{fp}, IP #{ip}, SP #{sp},\nDATA #{data},\nSTACK #{stack},\nCODE ["
-    buf << code.map { |k| format_instr_or_object(k) }.join(", ")
-    buf << "]"
+    buf << code.map { |k| format_instr_or_object(k) }.join(', ')
+    buf << ']'
     buf
   end
 
   def exec
-
     # fetch opcode
     opcode = code[ip]
 
@@ -108,8 +89,7 @@ class VM
       when Bytecodes::HALT
         # pass
       else
-        rv = Bytecodes::INVALID
-        raise "Invalid opcode #{opcode} at ip = #{ip-1}"
+        fail "Invalid opcode #{opcode} at ip = #{ip - 1}"
       end
 
       trace { dump_stack }
@@ -126,22 +106,21 @@ class VM
     Bytecodes::HALT
   end
 
-
   def display_instruction
     opcode = code[ip]
 
-    if (opcode.operand_count > 0)
-      start_idx = ip+1
-      end_idx = ip+opcode.operand_count
-      operands = (start_idx..end_idx).map { |idx| code[idx] }.join(", ")
+    if opcode.operand_count > 0
+      start_idx = ip + 1
+      end_idx = ip + opcode.operand_count
+      operands = (start_idx..end_idx).map { |idx| code[idx] }.join(', ')
     end
 
-    sprintf("%04d: %-10s %-10s",
-      ip, Bytecodes.to_instruction_from_opcode(opcode.opcode), operands)
+    format('%04d: %-10s %-10s',
+           ip, Bytecodes.to_instruction_from_opcode(opcode.opcode), operands)
   end
 
   def dump_stack
-    sprintf("\t%-10s", "stack=[ " + stack.map { |s| s }.join(", ") + " ]")
+    format("\t%-10s", 'stack=[ ' + stack.map { |s| s }.join(', ') + ' ]')
   end
 
   def dump_data_memory
@@ -149,7 +128,7 @@ class VM
     buf = "Data memory:\n"
     data.each do |d|
       addr += 1
-      buf << sprintf("%04d: %s\n", addr, d)
+      buf << format("%04d: %s\n", addr, d)
     end
     buf + "\n"
   end
@@ -158,11 +137,14 @@ class VM
     addr = 0
     buf = "Code memory:\n"
     code.each do |c|
-      buf << sprintf("%04d: %s\n", addr, format_instr_or_object(c))
+      buf << format("%04d: %s\n", addr, format_instr_or_object(c))
       addr += 1
     end
     buf + "\n"
   end
 
-
+  # add class instance variable
+  class << self
+    attr_accessor :tracing
+  end
 end
